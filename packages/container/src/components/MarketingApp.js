b8646dd4b6c55ce6104b06b9dab090803ae0060f
Jake Albert <jake@wisefour.com>
2021-12-16 19:28:44 +0900

    Added some navigation stuffs

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // Note that this technique can be used for a wide variety of technologies.
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname)
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
