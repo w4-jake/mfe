63a4c54d938e7647d3ab0fd2571ea86ac74bf82f
Jake Albert <jake@wisefour.com>
2021-12-16 15:22:52 +0900

    Add comment to container to enforce build action

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,7 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 
+// Note that this technique can be used for a wide variety of technologies.
 export default () => {
   const ref = useRef(null);
 
