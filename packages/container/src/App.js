79abab77e9fbb0a0becbf77f32d069d93abe32e2
Jake Albert <jake@wisefour.com>
2021-12-16 18:13:09 +0900

    Differentiate class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  // Want to keep this short for production...
+  productionPrefix: 'co'
+})
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
